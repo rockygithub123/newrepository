6e130694549439db849afa3b1e3c3f7ff996e522
Rakesh Babu KN <rakesh.sjp04@gmail.com>
2023-05-08 19:31:02 +0530

    script

---
+++ src/StaticAndNonStatic.java
@@ -0,0 +1,20 @@
+public class StaticAndNonStatic {
+   static int a1=9;
+    public static void main(String[] args) {
+        System.out.println("Am not good");
+        rocky();
+        new StaticAndNonStatic().ok();
+
+
+    }
+    public static void rocky()
+    {
+        int a=10;
+        int b=10;
+        System.out.println(a1+b);
+    }
+    public  void ok()
+    {
+        System.out.println(a1+a1);
+    }
+}
